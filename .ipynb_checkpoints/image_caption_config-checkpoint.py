fbf998bb182e5f3248e57f43eb629f404fc850c5
Charl Potgieter <charl.j.potgieter@gmail.com>
2023-04-27 11:38:56 +1000

    added config file

---
+++ .ipynb_checkpoints/image_caption_config-checkpoint.py
@@ -0,0 +1,9 @@
+config = {
+    'in_dir': r'D:\Onedrive\Documents_Charl\Computer_Technical\Programming_GitHub\AddImageCaption\Test_In_Folder',
+    
+    'out_dir': r'D:\Onedrive\Documents_Charl\Computer_Technical\Programming_GitHub\AddImageCaption\Test_Out_Folder',
+    
+    # Below Tuple contains percentage of frame, lower border, other border compared to longest side of image
+    # Border is area between frame and image
+    'image_ratios': (0.0075, 0.05, 0.03)
+}
---
+++ .ipynb_checkpoints/image_caption_config_template-checkpoint.py
@@ -0,0 +1,9 @@
+config = {
+    'in_dir': r'C:\SomeSourceFolder',
+    
+    'out_dir': r'C:\SomeTargetFolder',
+    
+    # Below Tuple contains percentage of frame, lower border, other border compared to longest side of image
+    # Border is area between frame and image
+    'image_ratios': (0.0075, 0.05, 0.03)
+}
