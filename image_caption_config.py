2337431db5aa9e2b994e75bdf1929e4f90863db6
Charl Potgieter <charl.j.potgieter@gmail.com>
2023-05-03 14:47:44 +1000

    update config

---
+++ image_caption_config.py
@@ -0,0 +1,15 @@
+config = {
+    'in_dir': '/SourceFolder',
+    
+    'out_dir': '/TargetFolder',
+    
+    # Below Tuple contains percentage of frame, lower border, other border compared to longest side of image
+    # Border is area between frame and image
+    'image_ratios': (0.0075, 0.05, 0.03), 
+    
+    'border_colour': 'RGB(220,220,220)',
+    
+    'frame_colour': 'RGB(120,75,50)',
+    
+    'font_colour': 'RGB(0,0,0)'
+}
