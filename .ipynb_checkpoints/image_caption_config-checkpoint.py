69f24e953a31ed0bd66e4dc8f40619eda871fbd1
Charl Potgieter <charl.j.potgieter@gmail.com>
2023-05-03 10:08:49 +1000

    Remove cache and checkpoints

--- .ipynb_checkpoints/image_caption_config-checkpoint.py
+++
@@ -1,9 +0,0 @@
-config = {
-    'in_dir': '/home/charl/OneDrive/Documents_Charl/Computer_Technical/Programming_GitHub/AddImageCaption/Test_In_Folder/',
-    
-    'out_dir': '/home/charl/OneDrive/Documents_Charl/Computer_Technical/Programming_GitHub/AddImageCaption/Test_Out_Folder/',
-    
-    # Below Tuple contains percentage of frame, lower border, other border compared to longest side of image
-    # Border is area between frame and image
-    'image_ratios': (0.0075, 0.05, 0.03)
-}
