3d9c8499f7447b3de3f2e82e1ab3a469fa974fb2
Charl Potgieter <charl.j.potgieter@gmail.com>
2023-05-06 09:59:52 +1000

    Tex aligned

--- image_caption_config.py
+++ image_caption_config.py
@@ -3,9 +3,9 @@ config = {
     
     'out_dir': '/TargetFolder',
     
-    # Below Tuple contains percentage of frame, lower border, other border compared to longest side of image
+    # Below Tuple contains percentage of frame and borders compared to longest side of image
     # Border is area between frame and image
-    'image_ratios': (0.0075, 0.05, 0.05), 
+    'image_ratios': (0.0075, 0.05), 
     
     'border_colour': 'RGB(220,220,220)',
     
